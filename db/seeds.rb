b502b7d10022d091920592c39bd743fbf05352dc
Steve Pesce <stevepesce879@gmail.com>
2020-09-28 21:53:04 -0400

    changed seed but only 1 standard user gets seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,11 @@
 require 'csv'
 require 'faker'
+#---------------------
+only_users = true;
+N = 1500
+#---------------------
 
-seedInput = ['full-destructive',1500]
-
-#TODO: convert this to boolean
-seedNumber = {
-  #'only-zips' => 0, not possible
-  'full-destructive' => 1,
-  'only-users' => 2
-}
-seed = seedNumber[seedInput[0]]
-N = seedInput[1]
-puts "seed option #{seed} user size: #{seedInput[1]}"
+puts "seed option #{only_users ? "only users" : "users and locations" } user size: #{N}"
 
 
 tables = 
@@ -24,23 +18,20 @@ tables =
   Match,
   User
 ]
-destroy_bar_size = tables.length + ((seed == 2 && Location.count < 10000) ? 0 : 10)
+destroy_bar_size = tables.length + ((only_users || Location.count < 1000) ? 0 : 10)
 destroy_bar = TTY::ProgressBar.new("Destroying Tables: [:bar]", total: destroy_bar_size)
 
 tables.each do |table| 
   table.destroy_all
   destroy_bar.advance
 end
-if(seed != 2)
+if(!only_users)
   Location.all.select(:id).find_in_batches(batch_size: Location.count/10) do |ids|
     Location.where(id: ids).delete_all
     destroy_bar.advance
   end 
   puts "finished destroying locations"
-end
 
-
-if(seed != 2)
   puts "seeding locations, this may take a minute...\n\n"
   Location.destroy_all
   zipBar = TTY::ProgressBar.new("Seeding Locations: [:bar]", total: 9)
@@ -73,7 +64,7 @@ puts "checking zip code errors..."
 errors = []
 zipCodes.each do |code|
   if !Location.find_by(zip: code)
-    errors.push("zip #{code} needs to be removed from db")
+    errors.push("zip #{code} was not found in the db")
   end
 end
 if !errors.empty?
@@ -144,99 +135,96 @@ def match(male,other)
     return ['male','female','any','other'].sample
   end
 end
-if seed > 0
-
-  careBar = TTY::ProgressBar.new("Seeding Caretakers [:bar]", total: 40)
-  10.times do |n|
-    caretributes =
-      {
-        first: Faker::Name.unique.first_name,
-        last: Faker::Name.last_name,
-        email: Faker::Internet.unique.safe_email,
-        password: 'care',
-        validated: true,
-        account_type: 'caretaker'
-      }
-      u = User.new(caretributes)
-      u.email = u.first + '@gmail.com'
-      u.save
-      careBar.advance(4) 
-  end
-  
-  barSize = N >= 40 ? 40 : N
-  usrBar = TTY::ProgressBar.new("Seeding Users [:bar]", total: barSize)
-  
-  N.times do |n|
-    usrBar.advance if (n % (N / barSize) == 0)
-    
-    male = (n <= N/2)
-    other = (n > (N - 10))  
-
-    attributes = 
+careBar = TTY::ProgressBar.new("Seeding Caretakers [:bar]", total: 40)
+10.times do |n|
+  caretributes =
     {
-      validated: true,
-      account_type: 'standard',
-      first: male ? Faker::Name.unique.male_first_name : Faker::Name.female_first_name,
+      first: Faker::Name.unique.first_name,
       last: Faker::Name.last_name,
       email: Faker::Internet.unique.safe_email,
-      password: 'qweasd123',
-      zip_code: zipCodes.sample,
-      gender: male ? 'male' : (other ? 'other' : 'female'),
-      match_gender: match(male,other),
+      password: 'care',
+      validated: true,
+      account_type: 'caretaker'
     }
-    u = User.new(attributes)
-    u.pic = pic_address('',u.gender,[0..100].sample)
+    u = User.new(caretributes)
     u.email = u.first + '@gmail.com'
     u.save
-    
-    myInterests = []
-    rand(2..5).times do
+    careBar.advance(4) 
+end
+
+barSize = N >= 40 ? 40 : N
+usrBar = TTY::ProgressBar.new("Seeding Users [:bar]", total: barSize)
+
+N.times do |n|
+  usrBar.advance if (n % (N / barSize) == 0)
+  
+  male = (n <= N/2)
+  other = (n > (N - 10))  
+
+  attributes = 
+  {
+    validated: true,
+    account_type: 'standard',
+    first: male ? Faker::Name.unique.male_first_name : Faker::Name.female_first_name,
+    last: Faker::Name.last_name,
+    email: Faker::Internet.unique.safe_email,
+    password: 'qweasd123',
+    zip_code: zipCodes.sample,
+    gender: male ? 'male' : (other ? 'other' : 'female'),
+    match_gender: match(male,other),
+  }
+  u = User.new(attributes)
+  u.pic = pic_address('',u.gender,[0..100].sample)
+  u.email = u.first + '@gmail.com'
+  puts ('error with user ' + attributes.first) if !u.valid?
+  u.save
+  
+  myInterests = []
+  rand(2..5).times do
+    interest = interests.sample
+    while (myInterests.include?(interest))
       interest = interests.sample
-      while (myInterests.include?(interest))
-        interest = interests.sample
-      end
-      myInterests.push(interest)    
-      u.interests.build(name: interest)
     end
-
-    u.disabilities.build(name: disabilities.sample)
-    u.save
-
-    meet = ['meet','find'].sample
-    looking_sentence = 
-    [
-      'I am looking forward to finding ',
-      'I want to ' + meet,
-      'I am here to ' + meet,
-      'I would like to ' + meet,
-      'I am hoping to ' + meet
-    ].sample  
-    myBio = ['Hello', 'Hola', 'Howdy', 'Hi there', 'Greetings'].sample + ', my name is ' + u.full_name 
-    if(!u.interests)
-      puts "user no interest #{u.full_name} + #{myInterests}"
-    end
-    myBio += ". My interests are #{u.interests_string}.  " + looking_sentence
-    myBio += " " + (u.match_gender == 'male' ? 'a guy' : (u.match_gender == 'female' ? 'a girl' : 'someone'))
-    
-    myBio += ". I am here for "
-    myBio +=
-    [
-      "finding a casual relationship!!!",
-      "finding a serious relationship.",
-      "finding friends!"
-    ].sample
-
-    u.bio = myBio
-    u.save
-    if(!u.location)
-      u.destroy
-      puts "#{n}th record created was bad and needs to be destroyed!"
-    end
-    if (n % 5) 
-      print '-'
-    end
+    myInterests.push(interest)    
+    u.interests.build(name: interest)
   end
-  print '---------->'
+
+  u.disabilities.build(name: disabilities.sample)
+  u.save
+
+  meet = ['meet','find'].sample
+  looking_sentence = 
+  [
+    'I am looking forward to finding ',
+    'I want to ' + meet,
+    'I am here to ' + meet,
+    'I would like to ' + meet,
+    'I am hoping to ' + meet
+  ].sample  
+  myBio = ['Hello', 'Hola', 'Howdy', 'Hi there', 'Greetings'].sample + ', my name is ' + u.full_name 
+  if(!u.interests)
+    puts "user no interest #{u.full_name} + #{myInterests}"
+  end
+  myBio += ". My interests are #{u.interests_string}.  " + looking_sentence
+  myBio += " " + (u.match_gender == 'male' ? 'a guy' : (u.match_gender == 'female' ? 'a girl' : 'someone'))
+  
+  myBio += ". I am here for "
+  myBio +=
+  [
+    "finding a casual relationship!!!",
+    "finding a serious relationship.",
+    "finding friends!"
+  ].sample
+
+  u.bio = myBio
+  u.save
+  if(!u.location)
+    puts "#{n}th record created was bad and needs to be destroyed, not associated with location!"
+  end
+  if (n % 5) 
+    
+  end
+
 
   puts 'seeding matches'
   reciever = User.all[15]
@@ -250,6 +238,7 @@ if seed > 0
   end
   puts "15th user got 10 match requests from his closest potential matches"
 end
+
 
 puts '-------------------------------------------------'
 puts '-----------SEEDING-------Successfull-------------'
